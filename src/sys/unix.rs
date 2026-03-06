4f9a19d693729521c1cafe11027838135d6eb36b
Peter Huene <peter@huene.dev>
2026-03-06 16:34:47 -0500

    Update src/sys/unix.rs
    Co-authored-by: Clay McLeod <3411613+claymcleod@users.noreply.github.com>

--- src/sys/unix.rs
+++ src/sys/unix.rs
@@ -7,6 +7,12 @@ use std::os::unix::fs::FileExt;
 /// Helper for writing to a file at a given offset.
 ///
 /// This is simply a wrapper around the `write_at` unix extension method.
-pub fn write_at(file: &File, buffer: &[u8], offset: u64) -> Result<usize> {
-    file.write_at(buffer, offset)
+pub fn write_at(file: &File, mut buffer: &[u8], mut offset: u64) -> Result<usize> {
+    let total = buffer.len();
+    while !buffer.is_empty() {
+        let n = file.write_at(buffer, offset)?;
+        buffer = &buffer[n..];
+        offset += n as u64;
+    }
+    Ok(total)
 }
